bb13c8a8cb68e6a368d6d4192a52d060bea39903
Thibault Deutsch <thibault.deutsch@gmail.com>
2015-02-13 01:40:30 +0200

    Configure xcode project

---
+++ Source/AlgoliaSearch.h
@@ -0,0 +1,19 @@
+//
+//  AlgoliaSearch.h
+//  AlgoliaSearch
+//
+//  Created by Thibault Deutsch on 13/02/15.
+//  Copyright (c) 2015 Algolia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlgoliaSearch.
+FOUNDATION_EXPORT double AlgoliaSearchVersionNumber;
+
+//! Project version string for AlgoliaSearch.
+FOUNDATION_EXPORT const unsigned char AlgoliaSearchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlgoliaSearch/PublicHeader.h>
+
+
