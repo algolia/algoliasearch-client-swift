4d21ac88ec7d2c173a5041c577546f42262616da
Thibault Deutsch <thibault.deutsch@gmail.com>
2015-06-26 17:39:51 +0200

    should be good now

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -o pipefail
+
 FILE=Tests/Helpers.swift
 
 if [[ $IOS = "TRUE" ]]; then
@@ -10,10 +13,10 @@ if [[ $IOS = "TRUE" ]]; then
     sed -ie "s/API_KEY_REPLACE_ME/${ALGOLIA_API_KEY}/g" $FILE
     sed -ie "s/JOB_NUMBER_REPLACE_ME/${TRAVIS_JOB_NUMBER}/g" $FILE
 
-    return xcodebuild -project AlgoliaSearch.xcodeproj -scheme "AlgoliaSearch iOS" \
+    xcodebuild -project AlgoliaSearch.xcodeproj -scheme "AlgoliaSearch iOS" \
         -sdk "$SDK" -destination "$VERSION" test | xcpretty -c
 else
     echo "Run OS X test..."
-    return xcodebuild -project AlgoliaSearch.xcodeproj -scheme "AlgoliaSearch OSX" \
+    xcodebuild -project AlgoliaSearch.xcodeproj -scheme "AlgoliaSearch OSX" \
         test | xcpretty -c
 fi
