52566a701111f22a95c414dca76004967ed045ef
Thibault Deutsch <thibault.deutsch@gmail.com>
2015-07-20 12:06:48 +0200

    activate code coverage

--- Source/Client.swift
+++ Source/Client.swift
@@ -75,13 +75,11 @@ public class Client : NSObject {
     /// - parameter tagFilters: value of the header X-Algolia-TagFilters
     /// - parameter userToken: value of the header X-Algolia-UserToken
     /// - parameter hostnames: the list of hosts that you have received for the service
-    public init(appID: String, apiKey: String, tagFilters: String? = nil, userToken: String? = nil, hostnames: [String]? = nil) {
-        if count(appID) == 0 {
-            NSException(name: "InvalidArgument", reason: "Application ID must be set", userInfo: nil).raise()
-        } else if count(apiKey) == 0 {
-            NSException(name: "InvalidArgument", reason: "APIKey must be set", userInfo: nil).raise()
+    public init(appID: String, apiKey: String, tagFilters: String? = nil, userToken: String? = nil, hostnames: [String]? = nil) throws {
+        guard appID != "" && apiKey != "" else {
+            throw NSError(domain: "appID and apiKey must be set", code: 403, userInfo: nil)
         }
-
+        
         self.appID = appID
         self.apiKey = apiKey
         self.tagFilters = tagFilters
@@ -124,8 +122,8 @@ public class Client : NSObject {
         manager = Manager(HTTPHeaders: HTTPHeaders)
     }
     
-    public class func clientWithAppID(appID: String, apiKey: String) -> Client {
-        return Client(appID: appID, apiKey: apiKey)
+    public class func clientWithAppID(appID: String, apiKey: String) throws -> Client {
+        return try Client(appID: appID, apiKey: apiKey)
     }
 
     /// Allow to set custom extra header.
