7083da0d10f0b690c865ee20cadde6c9113ce9e3
Thibault Deutsch <thibault.deutsch@gmail.com>
2015-07-29 11:44:17 +0200

    final fix travis

--- run_test.sh
+++ run_test.sh
@@ -7,14 +7,23 @@ FILE=Tests/Helpers.swift
 
 if [[ $IOS = "TRUE" ]]; then
     echo "Run iOS test..."
+    cp $FILE $FILE.bak
 
     echo "Replace env variable..."
-    sed -ie "s/APP_ID_REPLACE_ME/${ALGOLIA_APPLICATION_ID}/g" $FILE
-    sed -ie "s/API_KEY_REPLACE_ME/${ALGOLIA_API_KEY}/g" $FILE
-    sed -ie "s/JOB_NUMBER_REPLACE_ME/${TRAVIS_JOB_NUMBER}/g" $FILE
+    sed -i.tmp "s/APP_ID_REPLACE_ME/${ALGOLIA_APPLICATION_ID}/g" $FILE
+    sed -i.tmp "s/API_KEY_REPLACE_ME/${ALGOLIA_API_KEY}/g" $FILE
+
+    if ! [[ $TRAVIS_JOB_NUMBER && ${TRAVIS_JOB_NUMBER-_} ]]; then
+        echo "/!\ TRAVIS_JOB_NUMBER is not set."
+        TRAVIS_JOB_NUMBER=$RANDOM.$RANDOM
+    fi
+    sed -i.tmp "s/JOB_NUMBER_REPLACE_ME/${TRAVIS_JOB_NUMBER}/g" $FILE
 
     xcodebuild -project AlgoliaSearch.xcodeproj -scheme "AlgoliaSearch iOS" \
         -sdk "$SDK" -destination "$VERSION" test | xcpretty -c
+
+    mv $FILE.bak $FILE
+    rm $FILE.tmp
 else
     echo "Run OS X test..."
     xcodebuild -project AlgoliaSearch.xcodeproj -scheme "AlgoliaSearch OSX" \
