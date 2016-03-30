8ea9f8f486eb73d04fbbb457cb88e91425a5ea9e
Clément Le Provost <clement.leprovost@algolia.com>
2016-03-30 16:04:31 +0200

    Travis tests adapted to the new Cocoapods setup

--- run_test.sh
+++ run_test.sh
@@ -10,22 +10,27 @@ if ! [[ $TRAVIS_JOB_NUMBER && ${TRAVIS_JOB_NUMBER-_} ]]; then
     TRAVIS_JOB_NUMBER=$RANDOM.$RANDOM
 fi
 
+# Unit tests rely on Cocoapods.
+# If you wonder why, see `Podfile` for a discussion.
+pod install
+
 if [[ $IOS = "TRUE" ]]; then
     echo "Run iOS test..."
     cp $FILE $FILE.bak
 
+    # Environment variables are not available from the iOS Simulator, so we patch a source file instead.
     echo "Replace env variable..."
     sed -i.tmp "s/APP_ID_REPLACE_ME/${ALGOLIA_APPLICATION_ID}/g" $FILE
     sed -i.tmp "s/API_KEY_REPLACE_ME/${ALGOLIA_API_KEY}/g" $FILE
     sed -i.tmp "s/JOB_NUMBER_REPLACE_ME/${TRAVIS_JOB_NUMBER}/g" $FILE
 
-    xcodebuild -project AlgoliaSearch.xcodeproj -scheme "AlgoliaSearch iOS" \
+    xcodebuild -workspace AlgoliaSearch.xcworkspace -scheme "AlgoliaSearch-iOS-Tests" \
         -sdk "$SDK" -destination "$VERSION" test | xcpretty -c
 
     mv $FILE.bak $FILE
     rm $FILE.tmp
 else
     echo "Run OS X test..."
-    xcodebuild -project AlgoliaSearch.xcodeproj -scheme "AlgoliaSearch OSX" \
+    xcodebuild -workspace AlgoliaSearch.xcworkspace -scheme "AlgoliaSearch-OSX-Tests" \
         test | xcpretty -c
 fi
