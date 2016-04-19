1a830d850d32e98ee7e06a8f5f9f6a2c295d3bbb
Clément Le Provost <clement.leprovost@algolia.com>
2016-04-19 12:02:24 +0200

    [CI] Add tvOS target

--- run_test.sh
+++ run_test.sh
@@ -5,27 +5,40 @@ set -o pipefail
 
 FILE=Tests/Helpers.swift
 
+# Check environment variables.
+if [[ -z $PLATFORM ]]; then
+    echo "Please specify the platform" 1>&2
+    exit 1
+fi
+if [[ -z $SDK ]]; then
+    echo "Please specify the SDK" 1>&2
+    exit 1
+fi
 if ! [[ $TRAVIS_JOB_NUMBER && ${TRAVIS_JOB_NUMBER-_} ]]; then
     echo "/!\ TRAVIS_JOB_NUMBER is not set."
     TRAVIS_JOB_NUMBER=$RANDOM.$RANDOM
 fi
 
-if [[ $IOS = "TRUE" ]]; then
-    echo "Run iOS test..."
+# On platforms other than OS X, the tests run inside a simulator, so environment variables are not available;
+# let's patch the source file instead.
+if [[ "$PLATFORM" != "OSX" ]]; then
     cp $FILE $FILE.bak
 
     echo "Replace env variable..."
     sed -i.tmp "s/APP_ID_REPLACE_ME/${ALGOLIA_APPLICATION_ID}/g" $FILE
     sed -i.tmp "s/API_KEY_REPLACE_ME/${ALGOLIA_API_KEY}/g" $FILE
     sed -i.tmp "s/JOB_NUMBER_REPLACE_ME/${TRAVIS_JOB_NUMBER}/g" $FILE
+fi
 
-    xcodebuild -project AlgoliaSearch.xcodeproj -scheme "AlgoliaSearch iOS" \
-        -sdk "$SDK" -destination "$VERSION" test
+# Run tests.
+if [ -n "$DESTINATION" ]; then
+    xcodebuild -project AlgoliaSearch.xcodeproj -scheme "AlgoliaSearch $PLATFORM" -sdk "$SDK" -destination "$DESTINATION" test
+else
+    xcodebuild -project AlgoliaSearch.xcodeproj -scheme "AlgoliaSearch $PLATFORM" -sdk "$SDK" test
+fi
 
+# Clean-up: revert patched file.
+if [[ "$PLATFORM" != "OSX" ]]; then
     mv $FILE.bak $FILE
     rm $FILE.tmp
-else
-    echo "Run OS X test..."
-    xcodebuild -project AlgoliaSearch.xcodeproj -scheme "AlgoliaSearch OSX" \
-        test
 fi
