752b45fdc1f776fb2e26fba1c275c235bc508c3e
Guy Daher <guy.daher@algolia.com>
2017-08-08 11:16:26 +0200

    chore(travis): add travis_wait 30 to avoid timeout issues when running tests

--- tools/deploy-framework.sh
+++ tools/deploy-framework.sh
@@ -21,16 +21,17 @@ SELF_ROOT=`cd \`dirname "$0"\`; pwd`
 BUCKET_NAME="alg-binaries"
 DIST_OUT="$SELF_ROOT/Carthage/Build/iOS"
 
-# Carthage build
+# TODO: Carthage build
 # -------------------
 
+# TODO: Need to build for all flavors mac os etc so need more folders, also add the online and offline flavor
 
 # Retrieve version number from property list.
 
 VERSION_NO=`$SELF_ROOT/get-version.sh`
 echo "Version: $VERSION_NO"
 
-# Add a zip step here
+# TODO Add a zip step here
 
 MAIN_PACKAGE_BASENAME="$DEPLOY_NAME"
 MAIN_PACKAGE_NAME="$MAIN_PACKAGE_BASENAME.framework.zip"
@@ -39,7 +40,7 @@ MAIN_PACKAGE_NAME="$MAIN_PACKAGE_BASENAME.framework.zip"
 # if [[ -n $AWS_PROFILE ]]; then
 #     AWS_S3_CMD="aws s3 --profile $AWS_PROFILE"
 # else
-    AWS_S3_CMD="aws s3"
+AWS_S3_CMD="aws s3"
 # fi
 
 # Deploy to Amazon S3
@@ -50,8 +51,8 @@ echo "Deploying to Amazon S3:"
 echo "- Main package"
 DST_S3_DIR="s3://$BUCKET_NAME/$DEPLOY_NAME/$VERSION_NO"
 $AWS_S3_CMD cp \
-    "$DIST_OUT/$MAIN_PACKAGE_NAME" "$DST_S3_DIR/$MAIN_PACKAGE_NAME" \
-    --grants "read=uri=http://acs.amazonaws.com/groups/global/AllUsers"
+"$DIST_OUT/$MAIN_PACKAGE_NAME" "$DST_S3_DIR/$MAIN_PACKAGE_NAME" \
+--grants "read=uri=http://acs.amazonaws.com/groups/global/AllUsers"
 
 # List the destination to make sure everything is fine.
 echo "Contents of '$DST_S3_DIR':"
