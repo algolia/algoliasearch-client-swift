72679b43ed64771aa93dc4cae41e597cfa952088
Guy Daher <guy.daher@algolia.com>
2017-09-07 11:09:09 +0200

    feat(ios-swift-4): convert iOS target to Swift 4 syntax, and solve @objc inference warnings

--- Source/Helpers.swift
+++ Source/Helpers.swift
@@ -98,7 +98,7 @@ extension MutableCollection where Index == Int {
         for i in 0..<self.endIndex {
             let j = Int(arc4random_uniform(UInt32(self.endIndex - i))) + i
             guard i != j else { continue }
-            swap(&self[i], &self[j])
+            self.swapAt(i, j)
         }
     }
 }
