fe4018a78e203b65f207fe9c0dd141df2d0bb439
Guy Daher <guy.daher@algolia.com>
2017-10-12 16:34:41 +0200

    feat(swiftpm): rename name of library to AlgoliaSearch instead of algoliasearch-client-swift

--- Package.swift
+++ Package.swift
@@ -4,18 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "algoliasearch-client-swift",
+    name: "AlgoliaSearch",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "algoliasearch-client-swift",
-            targets: ["algoliasearch-client-swift"]),
+            name: "AlgoliaSearch",
+            targets: ["AlgoliaSearch"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "algoliasearch-client-swift",
+            name: "AlgoliaSearch",
             dependencies: [],
             path: "./Sources/AlgoliaSearch-Client")
     ]
