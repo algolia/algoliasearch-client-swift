ec3a666a60532fa22e194edbc986b20ac229020a
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-03-03 14:03:05 +0100

    Create StringWrapper protocol simplifying the creation of wrapped strings Add endpoints protocol + implementation

---
+++ Sources/AlgoliaSearchClientSwift/Endpoint/IndexingEndpoint.swift
@@ -0,0 +1,47 @@
+//
+//  File.swift
+//  
+//
+//  Created by Vladislav Fitc on 02/03/2020.
+//
+
+import Foundation
+
+typealias ResultCallback<T: Codable> = (Result<T, Error>) -> Void
+
+protocol IndexingEndpoint {
+  
+  /**
+   * Add a new record to an index.
+   * This method allows you to create records on your index by sending one or more objects.
+   * Each object contains a set of attributes and values, which represents a full record on an index.
+   * There is no limit to the number of objects that can be passed, but a size limit of 1 GB on the total request.
+   * For performance reasons, it is recommended to push batches of ~10 MB of payload.
+   * Batching records allows you to reduce the number of network calls required for multiple operations.
+   * But note that each indexed object counts as a single indexing operation.
+   * When adding large numbers of objects, or large sizes, be aware of our rate limit.
+   * You’ll know you’ve reached the rate limit when you start receiving errors on your indexing operations.
+   * This can only be resolved if you wait before sending any further indexing operations.
+   *
+   * - parameter record: The record of type T to save.
+   * - parameter requestOptions: Configure request locally with RequestOptions.
+   */
+  func saveObject<T: Codable>(record: T,
+                              requestOptions: RequestOptions?,
+                              completion: @escaping ResultCallback<ObjectCreation>)
+  
+  
+  /**
+   * Get one record using its ObjectID.
+   *
+   * - parameter objectID: The ObjectID to identify the record.
+   * - parameter attributesToRetrieve: Specify a list of Attribute to retrieve. This list will apply to all records.
+   * If you don’t specify any attributes, every attribute will be returned.
+   * - parameter requestOptions: Configure request locally with RequestOptions.
+   */
+  func getObject<R: Codable>(objectID: ObjectID,
+                             attributesToRetreive: [Attribute],
+                             requestOptions: RequestOptions?,
+                             completion: @escaping ResultCallback<R>)
+    
+}
---
+++ Sources/AlgoliaSearchClientSwift/Endpoint/SettingsEndpoint.swift
@@ -0,0 +1,35 @@
+//
+//  File.swift
+//  
+//
+//  Created by Vladislav Fitc on 02/03/2020.
+//
+
+import Foundation
+
+protocol SettingsEndpoint {
+  
+  /**
+   Get the Settings of an index.
+   - parameter requestOptions: Configure request locally with RequestOptions.
+   */
+  func getSettings(requestOptions: RequestOptions?,
+                   completion: @escaping ResultCallback<Settings>)
+
+  /**
+   Create or change an index’s Settings.
+   Only non-null settings are overridden; null settings are left unchanged
+   Performance wise, it’s better to setSettings before pushing the data.
+   
+   - parameter settings: The Settings to be set.
+   - parameter resetToDefault: Reset a settings to its default value.
+   - parameter forwardToReplicas: Whether to forward the same settings to the replica indices.
+   - parameter requestOptions: Configure request locally with RequestOptions.
+   */
+  func setSettings(_ settings: Settings,
+                   resetToDefault: [Settings.Key],
+                   forwardToReplicas: Bool?,
+                   requestOptions: RequestOptions?,
+                   completion: @escaping ResultCallback<RevisionIndex>)
+  
+}
