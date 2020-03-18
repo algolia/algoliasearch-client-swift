ced2e62ec527ad4ba6f15ce2eed32efc7e3b009b
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-03-18 12:15:19 +0100

    Fix typo

--- Sources/AlgoliaSearchClientSwift/Helpers/Decodable+String.swift
+++ Sources/AlgoliaSearchClientSwift/Helpers/Decodable+String.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-public extension JSON {
+public extension Decodable {
   
   init(jsonString: String) throws {
     let data = jsonString.data(using: .utf8)!
     let decoder = JSONDecoder()
-    self = try decoder.decode(JSON.self, from: data)
+    self = try decoder.decode(Self.self, from: data)
   }
   
 }
