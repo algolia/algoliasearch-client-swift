1bd117a100274138d1a646230a8f26bf36dcbe50
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-03-30 16:55:18 +0200

    Improve Batch request

--- Sources/AlgoliaSearchClientSwift/Models/Batch/BatchOperation.swift
+++ Sources/AlgoliaSearchClientSwift/Models/Batch/BatchOperation.swift
@@ -7,37 +7,44 @@
 
 import Foundation
 
-public struct BatchOperation<T: Codable>: Codable {
+public struct BatchOperation: Codable, Equatable {
 
   public let action: Action
-  public let body: T
-
+  public let body: JSON
+  
 }
 
-extension BatchOperation: Equatable where T: Equatable {}
+extension BatchOperation {
+  
+  init<T: Encodable>(action: Action, body: T) {
+    let body = try! JSON(body)
+    self.init(action: action, body: body)
+  }
+  
+}
 
 extension BatchOperation {
     
-  static func add(_ object: T) -> BatchOperation {
+  static func add<T: Encodable>(_ object: T) -> BatchOperation {
     return .init(action: .addObject, body: object)
   }
     
-  static func update<T: Codable>(objectID: ObjectID, _ object: T) -> BatchOperation<ObjectWrapper<T>> {
+  static func update<T: Codable>(objectID: ObjectID, _ object: T) -> BatchOperation {
     let objectWrapper = ObjectWrapper(objectID: objectID, object: object)
     return .init(action: .updateObject, body: objectWrapper)
   }
   
-  static func partialUpdate<T: Codable>(objectID: ObjectID, _ object: T, createIfNotExists: Bool) -> BatchOperation<ObjectWrapper<T>> {
+  static func partialUpdate<T: Codable>(objectID: ObjectID, _ object: T, createIfNotExists: Bool) -> BatchOperation {
     let objectWrapper = ObjectWrapper(objectID: objectID, object: object)
     return .init(action: createIfNotExists ? .partialUpdateObject : .partialUpdateObjectNoCreate, body: objectWrapper)
   }
   
-  static func partialUpdate(objectID: ObjectID, partialUpdate: PartialUpdate, createIfNotExists: Bool) -> BatchOperation<ObjectWrapper<PartialUpdate>> {
+  static func partialUpdate(objectID: ObjectID, partialUpdate: PartialUpdate, createIfNotExists: Bool) -> BatchOperation {
     let objectWrapper = ObjectWrapper(objectID: objectID, object: partialUpdate)
     return .init(action: createIfNotExists ? .partialUpdateObject : .partialUpdateObjectNoCreate, body: objectWrapper)
   }
 
-  static func delete(objectID: ObjectID) -> BatchOperation<ObjectWrapper<Empty>> {
+  static func delete(objectID: ObjectID) -> BatchOperation {
     return .init(action: .deleteObject, body: ObjectWrapper(objectID: objectID))
   }
   
