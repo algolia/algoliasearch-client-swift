6edd4f28ab0eb4f763be465abe09ec939dc6b0fd
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-04-07 19:05:36 +0200

    Remove unnecessary extension of ResultContainer

--- Sources/AlgoliaSearchClientSwift/Helpers/ResultContainer.swift
+++ Sources/AlgoliaSearchClientSwift/Helpers/ResultContainer.swift
@@ -14,10 +14,3 @@ public protocol ResultContainer {
   var result: Result<ResultValue, Swift.Error>? { get }
 
 }
-
-extension ResultContainer {
-  func getValue() throws -> ResultValue {
-    guard let result = result else { throw SyncOperationError.noResult }
-    return try result.value()
-  }
-}
