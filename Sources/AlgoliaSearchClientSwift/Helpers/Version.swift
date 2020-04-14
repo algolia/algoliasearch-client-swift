e5caf177aed19078b553e3361e8fc53b7c3d1bbb
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-04-14 21:50:28 +0200

    Implement UserAgent support

---
+++ Sources/AlgoliaSearchClientSwift/Helpers/Version.swift
@@ -0,0 +1,7 @@
+// This is generated file. Don't modify it manually.
+public struct Version {
+  public static let majorVersion: Int = 8
+  public static let minorVersion: Int = 0
+  public static let patchVersion: Int = 0
+  public static var description: String { return "\(majorVersion).\(minorVersion).\(patchVersion)"}
+}
