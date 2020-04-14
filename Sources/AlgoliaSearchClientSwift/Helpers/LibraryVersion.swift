0a7a016e708d3933a605a2cad03e0a56c01621b1
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-04-14 20:32:31 +0200

    Add LibraryVersion

---
+++ Sources/AlgoliaSearchClientSwift/Helpers/LibraryVersion.swift
@@ -0,0 +1,20 @@
+//
+//  LibraryVersion.swift
+//  
+//
+//  Created by Vladislav Fitc on 14/04/2020.
+//
+
+import Foundation
+
+public struct LibraryVersion {
+  
+  public let name: String
+  public let version: String
+  
+  public init(name: String, version: String) {
+    self.name = name
+    self.version = version
+  }
+  
+}
