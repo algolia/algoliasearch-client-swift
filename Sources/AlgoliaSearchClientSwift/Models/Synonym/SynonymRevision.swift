32497c3fd3556d4f9bfbf117dc4eb562bdabdab9
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-05-11 12:40:36 +0200

    Add synonym endpoint interface

---
+++ Sources/AlgoliaSearchClientSwift/Models/Synonym/SynonymRevision.swift
@@ -0,0 +1,10 @@
+//
+//  SynonymRevision.swift
+//  
+//
+//  Created by Vladislav Fitc on 11/05/2020.
+//
+
+import Foundation
+
+public struct SynonymRevision: Codable {}
---
+++ Sources/AlgoliaSearchClientSwift/Models/Synonym/SynonymSearchResponse.swift
@@ -0,0 +1,10 @@
+//
+//  SynonymSearchResponse.swift
+//  
+//
+//  Created by Vladislav Fitc on 11/05/2020.
+//
+
+import Foundation
+
+public struct SynonymSearchResponse: Codable {}
