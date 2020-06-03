46e310b6bda4d25de6ee0b7d8eb7ff14d2b1b52d
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-06-03 11:44:57 +0200

    Separate HTTP structures

---
+++ Sources/AlgoliaSearchClientSwift/Transport/HttpMethod.swift
@@ -0,0 +1,12 @@
+//
+//  HttpMethod.swift
+//  
+//
+//  Created by Vladislav Fitc on 02/06/2020.
+//
+
+import Foundation
+
+public enum HttpMethod: String {
+  case get = "GET", post = "POST", put = "PUT", delete = "DELETE"
+}
