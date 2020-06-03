aca301ac9fd898bcdf1a064e73710867f67c0fa8
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-06-03 11:44:57 +0200

    Separate HTTP structures

--- Sources/AlgoliaSearchClientSwift/Transport/Transport.swift
+++ Sources/AlgoliaSearchClientSwift/Transport/Transport.swift
@@ -7,29 +7,6 @@
 
 import Foundation
 
-/**
- * Indicate whether the HTTP call performed is of type [read] (GET) or [write] (POST, PUT ..).
- * Used to determine which timeout duration to use.
- */
-public enum CallType {
-    case read, write
-}
-
-extension CallType: CustomStringConvertible {
-  public var description: String {
-    switch self {
-    case .read:
-      return "read"
-    case .write:
-      return "write"
-    }
-  }
-}
-
-public enum HttpMethod: String {
-  case get = "GET", post = "POST", put = "PUT", delete = "DELETE"
-}
-
 protocol Transport: Credentials {
 
   func execute<Response: Codable, Output>(_ command: AlgoliaCommand, transform: @escaping (Response) -> Output, completion: @escaping (Result<Output, Error>) -> Void) -> Operation & TransportTask
