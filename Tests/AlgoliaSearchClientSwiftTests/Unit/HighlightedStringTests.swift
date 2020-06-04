1757c58bb688356d577fbe16ed52b101f7baea4e
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-06-05 01:32:24 +0200

    Migrate tests from Core

---
+++ Tests/AlgoliaSearchClientSwiftTests/Unit/HighlightedStringTests.swift
@@ -0,0 +1,38 @@
+//
+//  HighlightedStringTests.swift
+//  
+//
+//  Created by Vladislav Fitc on 05/06/2020.
+//
+
+import Foundation
+import XCTest
+@testable import AlgoliaSearchClientSwift
+
+class HighlightedStringTests: XCTestCase {
+  
+    func testWithDecodedString() throws {
+
+      let expectedHighlightedPart = "rennais"
+      
+      let inlineString = "VIDÉO. Des CRS déployés devant un lycée <em>rennais</em> pour les épreuves anticipées du bac"
+
+      let stringData = try Data(filename: "HighlightedString.json")
+      let decodedString = try XCTUnwrap(String(data: stringData, encoding: .utf8))
+
+      let inlineHiglighted = HighlightedString(string: inlineString)
+      let decodedHighlighted = HighlightedString(string: decodedString)
+      
+      func extractHighlightedPart(from title: HighlightedString) -> String {
+        let highlightedRange = title.taggedString.taggedRanges.first!
+        let highlightedPart = title.taggedString.output[highlightedRange]
+        return String(highlightedPart)
+      }
+      
+      XCTAssertEqual(expectedHighlightedPart, extractHighlightedPart(from: inlineHiglighted))
+      XCTAssertEqual(expectedHighlightedPart, extractHighlightedPart(from: decodedHighlighted))
+
+    }
+
+}
+
