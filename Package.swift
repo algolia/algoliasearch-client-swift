55cf20bb6b1309e15874656c371d79d9d8f1bb3d
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-07-10 21:33:27 +0200

    Update SwiftLog version

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
             targets: ["AlgoliaSearchClient"])
     ],
     dependencies: [
-        .package(url:"https://github.com/apple/swift-log.git", from: "1.2.0")
+        .package(url:"https://github.com/apple/swift-log.git", from: "1.3.0")
     ],
     targets: [
         .target(
