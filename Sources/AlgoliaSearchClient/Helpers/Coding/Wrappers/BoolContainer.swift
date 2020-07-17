b598c0a82b898304a589b52ac4af2c0baea503a1
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-07-18 00:42:27 +0200

    Add BoolContainer for advanced Bool decoding

---
+++ Sources/AlgoliaSearchClient/Helpers/Coding/Wrappers/BoolContainer.swift
@@ -0,0 +1,31 @@
+//
+//  BoolContainer.swift
+//  
+//
+//  Created by Vladislav Fitc on 17/07/2020.
+//
+
+import Foundation
+
+struct BoolContainer: RawRepresentable, Codable {
+  
+  let rawValue: Bool
+  
+  init(rawValue: Bool) {
+    self.rawValue = rawValue
+  }
+  
+  init(from decoder: Decoder) throws {
+    let container = try decoder.singleValueContainer()
+    if let boolValue = try? container.decode(Bool.self) {
+      self.rawValue = boolValue
+      return
+    }
+    if let boolFromString = Bool(try container.decode(String.self)) {
+      self.rawValue = boolFromString
+      return
+    }
+    throw DecodingError.dataCorruptedError(in: container, debugDescription: "Value cannot be decoded neither to Bool nor to String representing Bool value")
+  }
+  
+}
