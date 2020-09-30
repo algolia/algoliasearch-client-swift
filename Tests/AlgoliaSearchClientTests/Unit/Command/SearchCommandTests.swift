e150a0aee8a4d7c7b37f28ebbfd0888a1a6ef370
Vladislav Fitc <vladislav.fitz@gmail.com>
2020-09-30 19:00:34 +0300

    Fix browse command UT

--- Tests/AlgoliaSearchClientTests/Unit/Command/SearchCommandTests.swift
+++ Tests/AlgoliaSearchClientTests/Unit/Command/SearchCommandTests.swift
@@ -37,7 +37,7 @@ class SearchCommandTests: XCTestCase, AlgoliaCommandTest {
     let command = Command.Search.Browse(indexName: test.indexName, cursor: test.cursor, requestOptions: test.requestOptions)
     check(command: command,
           callType: .read,
-          method: .get,
+          method: .post,
           urlPath: "/1/indexes/testIndex/browse",
           queryItems: [.init(name: "testParameter", value: "testParameterValue")],
           body: CursorWrapper(test.cursor).httpBody,
