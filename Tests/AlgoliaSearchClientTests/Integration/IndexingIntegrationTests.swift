71d159eed21bf82e694bb0c81f34a37d412c2812
Vladislav Fitc <Vladislav.fitz@gmail.com>
2020-10-26 16:05:53 +0100

    chore: Add delete by tag CTS test. Add Builder conformance to DeleteBy… (#681)
    * feat: Add delete by tag CTS test. Add Builder conformance to DeleteByQuery.
    
    * chore: remove debugging lines

--- Tests/AlgoliaSearchClientTests/Integration/IndexingIntegrationTests.swift
+++ Tests/AlgoliaSearchClientTests/Integration/IndexingIntegrationTests.swift
@@ -28,10 +28,12 @@ class IndexingIntegrationTests: OnlineTestCase {
     let objectID = ObjectID(rawValue: .init(randomWithLength: 10))
     let object = TestRecord(objectID: objectID)
     
+    // Add 1 record with saveObject with an objectID and collect taskID/objectID
     let objectCreation = try index.saveObject(object)
     try objectCreation.wait()
     XCTAssertEqual(objectCreation.task.objectID, objectID)
     
+    // Add 1 record with saveObject without an objectID and collect taskID/objectID
     var objectWithGeneratedID = TestRecord()
     let objectWithGeneratedIDCreation = try index.saveObject(objectWithGeneratedID, autoGeneratingObjectID: true)
     try objectWithGeneratedIDCreation.wait()
@@ -39,9 +41,11 @@ class IndexingIntegrationTests: OnlineTestCase {
     let generatedObjectID = objectWithGeneratedIDCreation.task.objectID
     objectWithGeneratedID.objectID = generatedObjectID
             
+    // Perform a saveObjects with an empty set of objects and collect taskID
     let emptyObjectsCreation = try index.saveObjects([JSON]())
     try emptyObjectsCreation.wait()
-      
+    
+    // Add 2 records with saveObjects with an objectID and collect taskID/objectID
     let objectsIDs: [ObjectID] = [.init(rawValue: .init(randomWithLength: 10)), .init(rawValue: .init(randomWithLength: 10))]
     let objects = objectsIDs.map(TestRecord.init)
     let objectsCreation = try index.saveObjects(objects)
@@ -49,6 +53,7 @@ class IndexingIntegrationTests: OnlineTestCase {
 
     XCTAssertEqual(objectsCreation.batchesResponse.objectIDs, objectsIDs)
     
+    // Add 2 records with saveObjects without an objectID and collect taskID/objectID
     var objectsWithGeneratedID = [TestRecord(), TestRecord()]
     let objectsWithGeneratedIDCreation = try index.saveObjects(objectsWithGeneratedID, autoGeneratingObjectID: true)
     try objectWithGeneratedIDCreation.wait()
@@ -56,16 +61,19 @@ class IndexingIntegrationTests: OnlineTestCase {
     let generatedObjectIDs = objectsWithGeneratedIDCreation.batchesResponse.objectIDs.compactMap { $0 }
     objectsWithGeneratedID = zip(objectsWithGeneratedID, generatedObjectIDs).map { $0.0.set(\.objectID, to: $0.1) }
     
+    // Sequentially send 10 batches of 100 objects with objectID from 1 to 1000 with batch and collect taskIDs/objectIDs
     var batchRecords: [TestRecord] = []
     let chunkSize = 100
     let chunkCount = 10
     for startIndex in stride(from: 0, to: chunkSize * chunkCount, by: chunkSize) {
       let records = (startIndex..<startIndex + chunkSize).map { TestRecord(objectID: "\($0)") }
       batchRecords.append(contentsOf: records)
+      // Wait for all collected tasks to terminate with waitTask
       try index.saveObjects(records).wait()
     }
-    
-    
+
+    // Retrieve the 6 first records with getObject and check their content against original records
+    // Retrieve the 1000 remaining records with getObjects with objectIDs from 1 to 1000 and check their content against original records
     let firstObjectsIDs = [objectID, generatedObjectID] + objectsIDs + generatedObjectIDs
     
     let expectedObjects = [object, objectWithGeneratedID] + objects + objectsWithGeneratedID
@@ -74,6 +82,7 @@ class IndexingIntegrationTests: OnlineTestCase {
       XCTAssertEqual(expected, fetched)
     }
         
+    // Browse all records with browseObjects and make sure we have browsed 1006 records, and check that all objectIDs are found
     var response = try index.browse()
     
     var fetchedRecords: [TestRecord] = try response.extractHits()
@@ -94,22 +103,38 @@ class IndexingIntegrationTests: OnlineTestCase {
       XCTAssertEqual(value, fdict[key])
     }
     
+    // Alter 1 record with partialUpdateObject and collect taskID/objectID
     try index.partialUpdateObject(withID: objectID, with: .update(attribute: "string", value: "partiallyUpdated"), createIfNotExists: false).wait()
 
+    // Alter 2 records with partialUpdateObjects and collect taskID/objectID
     try index.partialUpdateObjects(updates: [(objectsIDs[0], .update(attribute: "string", value: "partiallyUpdated")), (objectsIDs[1], .increment(attribute: "numeric", value: 10))], createIfNotExists: false).wait()
     
-    
+    // Wait for all collected tasks to terminate with waitTask
+    // Retrieve all the previously altered records with getObject and check their content against the modified records
     let updated: [TestRecord] = try index.getObjects(withIDs: [objectID] + objectsIDs).results.compactMap { $0 }
     
     XCTAssertEqual(updated.first { $0.objectID == objectID }, object.set(\.string, to: "partiallyUpdated"))
     XCTAssertEqual(updated.first { $0.objectID == objectsIDs.first! }, objects.first!.set(\.string, to: "partiallyUpdated"))
     XCTAssertEqual(updated.first { $0.objectID == objectsIDs.last! }, objects.last!.set(\.numeric, to: objects.last!.numeric + 10))
     
+    // Add 1 record with saveObject with an objectID and a tag algolia and wait for the task to finish
+    let taggedRecord = TestRecord(objectID: "taggedObject").set(\._tags, to: ["algolia"])
+    try index.saveObject(taggedRecord).wait()
+    
+    // Delete the first record with deleteObject and collect taskID
     try index.deleteObject(withID: objectID).wait()
+    
+    // Delete the record containing the tag algolia with deleteBy and the tagFilters option and collect taskID
+    try index.deleteObjects(byQuery: DeleteByQuery().set(\.filters, to: "algolia")).wait()
+        
+    // Delete the 5 remaining first records with deleteObjects and collect taskID
     try index.deleteObjects(withIDs: [generatedObjectID] + objectsIDs + generatedObjectIDs).wait()
+    
+    // Delete the 1000 remaining records with clearObjects and collect taskID
+    // Wait for all collected tasks to terminate
     try index.clearObjects().wait()
     
-    
+    // Browse all objects with browseObjects and make sure that no records are returned
     response = try index.browse()
     XCTAssertEqual(response.nbHits, 0)
     
