785b1e2ff45716a9f457812e60fe1293d428d802
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-11-13 18:00:40 +0100

    Generate random string using modern APIs

--- Tests/AlgoliaSearchClientTests/Helper/String+Random.swift
+++ Tests/AlgoliaSearchClientTests/Helper/String+Random.swift
@@ -10,15 +10,8 @@ import Foundation
 extension String {
   
   init(randomWithLength length: Int) {
-    let letters : NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    let len = UInt32(letters.length)
-    var randomString = ""
-    for _ in 0 ..< length {
-        let rand = arc4random_uniform(len)
-        var nextChar = letters.character(at: Int(rand))
-        randomString += NSString(characters: &nextChar, length: 1) as String
-    }
-    self = randomString
+    let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    self = String((0..<length).compactMap { _ in letters.randomElement() })
   }
   
   static var random: String { .random(length: .random(in: 1...30)) }
