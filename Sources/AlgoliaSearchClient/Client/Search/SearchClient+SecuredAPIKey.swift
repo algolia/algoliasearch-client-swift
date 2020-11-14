acd8eafbd692996930a18856028e7fd3bb927c65
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-11-14 01:02:39 +0100

    Implement hmac256 digest generation for linux using Swift Crypto

--- Sources/AlgoliaSearchClient/Client/Search/SearchClient+SecuredAPIKey.swift
+++ Sources/AlgoliaSearchClient/Client/Search/SearchClient+SecuredAPIKey.swift
@@ -7,30 +7,14 @@
 
 import Foundation
 
-#if os(Linux)
-import Crypto
-#else
-import CommonCrypto
-#endif
-
 public extension SearchClient {
 
-  #if os(Linux)
   func generateSecuredApiKey(parentApiKey: APIKey,
                              with restriction: SecuredAPIKeyRestriction) -> APIKey {
     let queryParams = restriction.urlEncodedString
-    let hash = queryParams.hmac(.bits256)
+    let hash = queryParams.hmac256(withKey: parentApiKey.rawValue)
     return APIKey(rawValue: "\(hash)\(queryParams)".toBase64())
   }
-  #else
-
-  func generateSecuredApiKey(parentApiKey: APIKey,
-                             with restriction: SecuredAPIKeyRestriction) -> APIKey {
-    let queryParams = restriction.urlEncodedString
-    let hash = queryParams.hmac(algorithm: .sha256, key: parentApiKey.rawValue)
-    return APIKey(rawValue: "\(hash)\(queryParams)".toBase64())
-  }
-  #endif
 
   func getSecuredApiKeyRemainingValidity(_ securedAPIKey: APIKey) -> TimeInterval? {
     guard let rawDecodedAPIKey = securedAPIKey.rawValue.fromBase64() else { return nil }
--- Sources/AlgoliaSearchClient/Helpers/Crypto/String+HMAC.swift
+++ Sources/AlgoliaSearchClient/Helpers/Crypto/String+HMAC.swift
@@ -14,21 +14,18 @@ import CommonCrypto
 #endif
 
 extension String {
-
-  #if os(Linux)
-  public func hmac(_ key: SymmetricKeySize) -> String {
-    let sk = SymmetricKey(size: key)
-    let data = self.data(using: .utf8)!
-    let mac = HMAC<SHA512>.authenticationCode(for: data, using: sk)
-    return String(describing: mac)
-  }
-  #else
-  func hmac(algorithm: HmacAlgorithm, key: String) -> String {
+  
+  func hmac256(withKey key: String) -> String {
+    #if os(Linux)
+    let key = SymmetricKey(data: key.data(using: .utf8)!)
+    let digest = Array(HMAC<SHA256>.authenticationCode(for: data(using: .utf8)!, using: key))
+    #else
+    let algorithm = HmacAlgorithm.sha256
     var digest = [UInt8](repeating: 0, count: algorithm.digestLength)
     CCHmac(algorithm.algorithm, key, key.count, self, self.count, &digest)
+    #endif
     let data = Data(digest)
     return data.map { String(format: "%02hhx", $0) }.joined()
   }
-  #endif
 
 }
