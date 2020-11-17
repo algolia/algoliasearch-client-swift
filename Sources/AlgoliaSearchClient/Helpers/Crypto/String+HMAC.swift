a919c6dabd2bd69d9e629237eb10fa18c977bb5c
Vladislav Fitc <vladislav.fitc@algolia.com>
2020-11-17 17:36:09 +0100

    Replace implicit unwrapping string to data transformation

--- Sources/AlgoliaSearchClient/Helpers/Crypto/String+HMAC.swift
+++ Sources/AlgoliaSearchClient/Helpers/Crypto/String+HMAC.swift
@@ -17,8 +17,8 @@ extension String {
   
   func hmac256(withKey key: String) -> String {
     #if os(Linux)
-    let key = SymmetricKey(data: key.data(using: .utf8)!)
-    let digest = Array(HMAC<SHA256>.authenticationCode(for: data(using: .utf8)!, using: key))
+    let key = SymmetricKey(data: Data(key.utf8))
+    let digest = Array(HMAC<SHA256>.authenticationCode(for: Data(utf8), using: key))
     #else
     let algorithm = HmacAlgorithm.sha256
     var digest = [UInt8](repeating: 0, count: algorithm.digestLength)
