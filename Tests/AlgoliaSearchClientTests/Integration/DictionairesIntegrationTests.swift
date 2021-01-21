da6c037333831b56339f26aa84efdc4507463fb8
Vladislav Fitc <vladislav.fitc@algolia.com>
2021-01-21 14:02:00 +0100

    Add Dictionary integration test template

---
+++ Tests/AlgoliaSearchClientTests/Integration/DictionairesIntegrationTests.swift
@@ -0,0 +1,26 @@
+//
+//  DictionairesIntegrationTests.swift
+//  
+//
+//  Created by Vladislav Fitc on 21/01/2021.
+//
+
+import Foundation
+import XCTest
+@testable import AlgoliaSearchClient
+
+class DictionairesIntegrationTests: OnlineTestCase {
+  
+  func stopwordsDictionaryTests() {
+    
+  }
+  
+  func compoundsDictionaryTests() {
+    
+  }
+  
+  func pluralsDictionaryTests() {
+    
+  }
+  
+}
