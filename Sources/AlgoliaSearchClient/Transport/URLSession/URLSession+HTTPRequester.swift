63486684bf258de3269384583554d6db4b15c9bb
Vladislav Fitc <vladislav.fitc@algolia.com>
2021-01-25 18:07:40 +0100

    Improve HTTPRequester implementation logging

--- Sources/AlgoliaSearchClient/Transport/URLSession/URLSession+HTTPRequester.swift
+++ Sources/AlgoliaSearchClient/Transport/URLSession/URLSession+HTTPRequester.swift
@@ -17,8 +17,7 @@ extension URLSession: HTTPRequester {
   public func perform<T: Decodable>(request: URLRequest, completion: @escaping (Result<T, Error>) -> Void) -> TransportTask {
     let task = dataTask(with: request) { (data, response, error) in
       let result = Result<T, Error>(data: data, response: response, error: error)
-      if case .success = result,
-        let data = data,
+      if let data = data,
         let responseJSON = try? JSONDecoder().decode(JSON.self, from: data) {
         Logger.loggingService.log(level: .debug, message: "Response: \(responseJSON)")
       }
