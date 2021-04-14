33d7898388d02cbef68b4960d6064f0ba4a60b5d
Vladislav Fitc <vladislav.fitc@algolia.com>
2021-04-14 13:12:14 +0200

    change FacetOrderContainer format

--- Sources/AlgoliaSearchClient/Models/Search/Response/SearchResponse/Auxiliary/RenderingContent/FacetOrderContainer.swift
+++ Sources/AlgoliaSearchClient/Models/Search/Response/SearchResponse/Auxiliary/RenderingContent/FacetOrderContainer.swift
@@ -9,10 +9,45 @@ import Foundation
 
 public struct FacetOrderContainer: Codable {
       
-  public let facetOrder: [AttributedFacets]
+  public let facetOrdering: FacetOrdering
   
-  public init(facetOrder: [AttributedFacets] = []) {
-    self.facetOrder = facetOrder
+  public init(facetOrdering: FacetOrdering = .init()) {
+    self.facetOrdering = facetOrdering
+  }
+  
+}
+
+public struct FacetOrdering: Codable {
+  
+  public let facets: OrderingRule
+  public let facetValues: [String: OrderingRule]
+  
+  public init(facets: OrderingRule = .init(),
+              facetValues: [String: OrderingRule] = [:]) {
+    self.facets = facets
+    self.facetValues = facetValues
+  }
+  
+}
+
+
+public struct OrderingRule: Codable {
+  
+  public let order: [String]?
+  public let hide: [String]?
+  public let sortBy: SortRule?
+  
+  public enum SortRule: String, Codable {
+    case alpha
+    case count
+  }
+  
+  public init(order: [String] = ["*"],
+              hide: [String]? = nil,
+              sortBy: SortRule? = nil) {
+    self.order = order
+    self.hide = hide
+    self.sortBy = sortBy
   }
   
 }
