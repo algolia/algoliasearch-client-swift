84122d38e0fd7954fdef4fc4b176742224e591aa
Vladislav Fitc <vladislav.fitz@gmail.com>
2021-09-01 15:25:16 +0300

    Implement recommend client tests

---
+++ Tests/AlgoliaSearchClientTests/Unit/RecommendClientTests.swift
@@ -0,0 +1,115 @@
+//
+//  RecommendClientTests.swift
+//  
+//
+//  Created by Vladislav Fitc on 01/09/2021.
+//
+
+import Foundation
+import XCTest
+@testable import AlgoliaSearchClient
+
+class RecommendClientTests: XCTestCase {
+  
+  func testRecommendations() {
+    let requester = TestRequester()
+    let client = RecommendClient(configuration: .init(applicationID: "test-app-id", apiKey: "test-api-key"), requester: requester)
+    
+    let exp = expectation(description: "request")
+    
+    requester.onRequest = { request in
+      XCTAssertEqual(request.url?.absoluteString, "https://test-app-id-dsn.algolia.net/1/indexes/*/recommendations?")
+      XCTAssertEqual(request.value(forHTTPHeaderField: "Content-Type"), "application/json")
+      XCTAssertEqual(request.value(forHTTPHeaderField: "X-Algolia-Application-Id"), "test-app-id")
+      XCTAssertEqual(request.value(forHTTPHeaderField: "X-Algolia-API-Key"), "test-api-key")
+      XCTAssertEqual(request.httpMethod, "POST")
+      XCTAssertNotNil(request.httpBody)
+      if let body = request.httpBody {
+        AssertMatch(body, ["requests": [
+          [
+            "model" : "bought-together",
+            "objectID" : "B018APC4LE",
+            "indexName" : "IndexName",
+            "threshold" : 0,
+          ]]
+        ])
+      }
+      exp.fulfill()
+    }
+    
+    client.getRecommendations(options: [.init(indexName: "IndexName",
+                                              model: .boughtTogether,
+                                              objectID: "B018APC4LE")]) { _ in }
+    
+    waitForExpectations(timeout: 5, handler: nil)
+
+  }
+  
+  func testFrequentlyBoughtTogether() {
+    
+    let requester = TestRequester()
+    let client = RecommendClient(configuration: .init(applicationID: "test-app-id", apiKey: "test-api-key"), requester: requester)
+    
+    let exp = expectation(description: "request")
+    
+    requester.onRequest = { request in
+      XCTAssertEqual(request.url?.absoluteString, "https://test-app-id-dsn.algolia.net/1/indexes/*/recommendations?")
+      XCTAssertEqual(request.value(forHTTPHeaderField: "Content-Type"), "application/json")
+      XCTAssertEqual(request.value(forHTTPHeaderField: "X-Algolia-Application-Id"), "test-app-id")
+      XCTAssertEqual(request.value(forHTTPHeaderField: "X-Algolia-API-Key"), "test-api-key")
+      XCTAssertEqual(request.httpMethod, "POST")
+      XCTAssertNotNil(request.httpBody)
+      if let body = request.httpBody {
+        AssertMatch(body, ["requests": [
+          [
+            "model" : "bought-together",
+            "objectID" : "B018APC4LE",
+            "indexName" : "IndexName",
+            "threshold" : 0,
+          ]]
+        ])
+      }
+      exp.fulfill()
+    }
+    
+    client.getFrequentlyBoughtTogether(options: [.init(indexName: "IndexName", objectID: "B018APC4LE")]) { _ in }
+    
+    waitForExpectations(timeout: 5, handler: nil)
+    
+  }
+  
+  func testRelatedProducts() {
+    
+    let requester = TestRequester()
+    let client = RecommendClient(configuration: .init(applicationID: "test-app-id", apiKey: "test-api-key"), requester: requester)
+    
+    let exp = expectation(description: "request")
+    
+    requester.onRequest = { request in
+      XCTAssertEqual(request.url?.absoluteString, "https://test-app-id-dsn.algolia.net/1/indexes/*/recommendations?")
+      XCTAssertEqual(request.value(forHTTPHeaderField: "Content-Type"), "application/json")
+      XCTAssertEqual(request.value(forHTTPHeaderField: "X-Algolia-Application-Id"), "test-app-id")
+      XCTAssertEqual(request.value(forHTTPHeaderField: "X-Algolia-API-Key"), "test-api-key")
+      XCTAssertEqual(request.httpMethod, "POST")
+      XCTAssertNotNil(request.httpBody)
+      if let body = request.httpBody {
+        AssertMatch(body, ["requests": [
+          [
+            "model" : "related-products",
+            "objectID" : "B018APC4LE",
+            "indexName" : "IndexName",
+            "threshold" : 0,
+          ]]
+        ])
+      }
+      exp.fulfill()
+    }
+    
+    client.getRelatedProducts(options: [.init(indexName: "IndexName", objectID: "B018APC4LE")]) { _ in }
+    
+    waitForExpectations(timeout: 5, handler: nil)
+    
+  }
+
+  
+}
---
+++ Tests/AlgoliaSearchClientTests/Unit/TestRequester.swift
@@ -0,0 +1,29 @@
+//
+//  TestRequester.swift
+//  
+//
+//  Created by Vladislav Fitc on 01/09/2021.
+//
+
+import Foundation
+@testable import AlgoliaSearchClient
+
+class TestRequester: HTTPRequester {
+  
+  var onRequest: (URLRequest) -> Void = { _ in }
+  
+  func perform<T>(request: URLRequest, completion: @escaping (Result<T, Error>) -> Void) -> TransportTask where T : Decodable {
+    onRequest(request)
+    return MockTask()
+  }
+  
+  class MockTask: NSObject, TransportTask {
+    
+    func cancel() {}
+    
+    var progress: Progress {
+      return .init(totalUnitCount: 0)
+    }
+  }
+  
+}
