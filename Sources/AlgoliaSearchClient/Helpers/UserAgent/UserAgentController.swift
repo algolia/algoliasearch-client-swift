bd5807fb8261378a1ca1c60e53491290757c83d0
Vladislav Fitc <vladislav.fitc@algolia.com>
2021-10-06 19:12:20 +0200

    Replace static UserAgent with UserAgent extensions

--- Sources/AlgoliaSearchClient/Helpers/UserAgent/UserAgentController.swift
+++ Sources/AlgoliaSearchClient/Helpers/UserAgent/UserAgentController.swift
@@ -9,11 +9,32 @@ import Foundation
 
 public struct UserAgentController {
 
-  public internal(set) static var userAgents: [UserAgent] = [.operatingSystem, .library]
+  public static var userAgents: [UserAgent] {
+    extensions.compactMap { $0 as? UserAgent }
+  }
+  
+  public internal(set) static var extensions: [UserAgentExtending] = [UserAgent.operatingSystem, UserAgent.library]
+  
+  public static var httpHeaderValue: String {
+    return extensions.map(\.userAgentExtension).joined(separator: "; ")
+  }
 
-  /// Append user agent info which will be includede in each API call.
+  /// Append user agent to include into each API call.
+  @available(*, deprecated, message: "Use `append(_ userAgentExtension: UserAgentExtending)` instead")
   public static func append(userAgent: UserAgent) {
-    userAgents.append(userAgent)
+    extensions.append(userAgent)
+  }
+  
+  /// Append user agent to include into each API call.
+  public static func append(_ userAgentExtension: UserAgentExtending) {
+    extensions.append(userAgentExtension)
   }
 
 }
+
+public protocol UserAgentExtending {
+  
+  var userAgentExtension: String { get }
+  
+}
+
