b2a82baf991577707b9d429e0353d9c09b98e0bd
Vladislav Fitc <vladislav.fitc@algolia.com>
2022-10-24 16:07:43 +0200

    feat(search): Disjunctive Faceting (#802)

---
+++ Sources/AlgoliaSearchClient/Helpers/DisjunctiveFacetingHelper.swift
@@ -0,0 +1,137 @@
+//
+//  DisjunctiveFacetingHelper.swift
+//  
+//
+//  Created by Vladislav Fitc on 19/10/2022.
+//
+
+import Foundation
+
+/// Helper making multiple queries for disjunctive faceting
+/// and merging the multiple search responses into a single one with
+/// combined facets information
+struct DisjunctiveFacetingHelper {
+
+  let query: Query
+  let refinements: [Attribute: [String]]
+  let disjunctiveFacets: Set<Attribute>
+
+  init(query: Query,
+       refinements: [Attribute: [String]],
+       disjunctiveFacets: Set<Attribute>) {
+    self.query = query
+    self.refinements = refinements
+    self.disjunctiveFacets = disjunctiveFacets
+  }
+
+  /// Build filters SQL string from the provided refinements and disjunctive facets set
+  func buildFilters(excluding excludedAttribute: Attribute?) -> String {
+    String(
+      refinements
+        .sorted(by: { $0.key.rawValue < $1.key.rawValue })
+        .filter { (name: Attribute, values: [String]) in
+        name != excludedAttribute && !values.isEmpty
+      }.map { (name: Attribute, values: [String]) in
+        let facetOperator = disjunctiveFacets.contains(name) ? " OR " : " AND "
+        let expression = values
+          .map { value in """
+          "\(name)":"\(value)"
+          """
+          }
+          .joined(separator: facetOperator)
+        return "(\(expression))"
+      }.joined(separator: " AND ")
+    )
+  }
+
+  /// Get applied disjunctive facet values for provided attribute
+  func appliedDisjunctiveFacetValues(for attribute: Attribute) -> Set<String> {
+    guard disjunctiveFacets.contains(attribute) else {
+      return []
+    }
+    return refinements[attribute].flatMap(Set.init) ?? []
+  }
+
+  /// Build search queries to fetch the necessary facets information for disjunctive faceting
+  /// If the disjunctive facets set is empty, makes a single request with applied conjunctive filters
+  func makeQueries() -> [Query] {
+    var queries = [Query]()
+
+    var mainQuery = query
+    mainQuery.filters = buildFilters(excluding: .none)
+
+    queries.append(mainQuery)
+
+    disjunctiveFacets
+      .sorted(by: { $0.rawValue < $1.rawValue })
+      .forEach { disjunctiveFacet in
+      var disjunctiveQuery = query
+      disjunctiveQuery.facets = [disjunctiveFacet]
+      disjunctiveQuery.filters = buildFilters(excluding: disjunctiveFacet)
+      disjunctiveQuery.hitsPerPage = 0
+      disjunctiveQuery.attributesToRetrieve = []
+      disjunctiveQuery.attributesToHighlight = []
+      disjunctiveQuery.attributesToSnippet = []
+      disjunctiveQuery.analytics = false
+      queries.append(disjunctiveQuery)
+    }
+
+    return queries
+  }
+
+  /// Merge received search responses into single one with combined facets information
+  func mergeResponses(_ responses: [SearchResponse], keepSelectedEmptyFacets: Bool = true) throws -> SearchResponse {
+    guard var mainResponse = responses.first else {
+      throw DisjunctiveFacetingError.emptyResponses
+    }
+
+    let responsesForDisjunctiveFaceting = responses.dropFirst()
+
+    var mergedDisjunctiveFacets = [Attribute: [Facet]]()
+    var mergedFacetStats = mainResponse.facetStats ?? [:]
+    var mergedExhaustiveFacetsCount = mainResponse.exhaustiveFacetsCount ?? true
+
+    for result in responsesForDisjunctiveFaceting {
+      // Merge facet values
+      if let facetsPerAttribute = result.facets {
+        for (attribute, facets) in facetsPerAttribute {
+          // Complete facet values applied in the filters
+          // but missed in the search response
+          let missingFacets = appliedDisjunctiveFacetValues(for: attribute)
+            .subtracting(facets.map(\.value))
+            .map { Facet(value: $0, count: 0) }
+          mergedDisjunctiveFacets[attribute] = facets + missingFacets
+        }
+      }
+      // Merge facets stats
+      if let facetStats = result.facetStats {
+        mergedFacetStats.merge(facetStats) { _, last in last }
+      }
+      // If facet counts are not exhaustive, propagate this information to the main results.
+      // Because disjunctive queries are less restrictive than the main query, it can happen that the main query
+      // returns exhaustive facet counts, while the disjunctive queries do not.
+      if let exhaustiveFacetsCount = result.exhaustiveFacetsCount {
+        mergedExhaustiveFacetsCount = mergedExhaustiveFacetsCount && exhaustiveFacetsCount
+      }
+    }
+    mainResponse.disjunctiveFacets = mergedDisjunctiveFacets
+    mainResponse.facetStats = mergedFacetStats
+    mainResponse.exhaustiveFacetsCount = mergedExhaustiveFacetsCount
+
+    return mainResponse
+  }
+
+}
+
+public enum DisjunctiveFacetingError: LocalizedError {
+
+  case emptyResponses
+
+  var localizedDescription: String {
+    switch self {
+    case .emptyResponses:
+      return "Unexpected empty search responses list. At least one search responses might be present."
+    }
+  }
+
+}
---
+++ Tests/AlgoliaSearchClientTests/Unit/DisjunctiveFacetingHelperTests.swift
@@ -0,0 +1,386 @@
+//
+//  DisjunctiveFacetingHelperTests.swift
+//
+//
+//  Created by Vladislav Fitc on 20/03/2020.
+//
+
+import Foundation
+import XCTest
+@testable import AlgoliaSearchClient
+
+class DisjunctiveFacetingHelperTests: XCTestCase {
+  
+  func testBuildFilters() throws {
+    let refinements: [Attribute: [String]] = [
+      "size": ["m", "s"],
+      "color": ["blue", "green", "red"],
+      "brand": ["apple", "samsung", "sony"]
+    ]
+    
+    let disjunctiveFacets: Set<Attribute> = [
+      "color"
+    ]
+    
+    let helper = DisjunctiveFacetingHelper(query: Query(),
+                                           refinements: refinements,
+                                           disjunctiveFacets: disjunctiveFacets)
+    
+    XCTAssertEqual(helper.buildFilters(excluding: .none), """
+    ("brand":"apple" AND "brand":"samsung" AND "brand":"sony") AND ("color":"blue" OR "color":"green" OR "color":"red") AND ("size":"m" AND "size":"s")
+    """)
+    XCTAssertEqual(helper.buildFilters(excluding: "popularity"), """
+    ("brand":"apple" AND "brand":"samsung" AND "brand":"sony") AND ("color":"blue" OR "color":"green" OR "color":"red") AND ("size":"m" AND "size":"s")
+    """)
+    XCTAssertEqual(helper.buildFilters(excluding: "brand"), """
+    ("color":"blue" OR "color":"green" OR "color":"red") AND ("size":"m" AND "size":"s")
+    """)
+    XCTAssertEqual(helper.buildFilters(excluding: "color"), """
+    ("brand":"apple" AND "brand":"samsung" AND "brand":"sony") AND ("size":"m" AND "size":"s")
+    """)
+    XCTAssertEqual(helper.buildFilters(excluding: "size"), """
+    ("brand":"apple" AND "brand":"samsung" AND "brand":"sony") AND ("color":"blue" OR "color":"green" OR "color":"red")
+    """)
+  }
+  
+  func testAppliedDisjunctiveFacetValues() {
+    let refinements: [Attribute: [String]] = [
+      "size": ["m", "s"],
+      "color": ["blue", "green", "red"],
+      "brand": ["apple", "samsung", "sony"]
+    ]
+
+    let disjunctiveFacets: Set<Attribute> = [
+      "color",
+      "brand"
+    ]
+    
+    let helper = DisjunctiveFacetingHelper(query: Query(),
+                                           refinements: refinements,
+                                           disjunctiveFacets: disjunctiveFacets)
+    
+    XCTAssertTrue(helper.appliedDisjunctiveFacetValues(for: "popularity").isEmpty)
+    XCTAssertTrue(helper.appliedDisjunctiveFacetValues(for: "size").isEmpty)
+    XCTAssertEqual(helper.appliedDisjunctiveFacetValues(for: "color"), ["red", "green", "blue"])
+    XCTAssertEqual(helper.appliedDisjunctiveFacetValues(for: "brand"), ["samsung", "sony", "apple"])
+  }
+  
+  func testMakeQueriesNoDisjunctive() {
+    let refinements: [Attribute: [String]] = [
+      "size": ["m", "s"],
+      "color": ["blue", "green", "red"],
+      "brand": ["apple", "samsung", "sony"]
+    ]
+
+    let helper = DisjunctiveFacetingHelper(query: Query(),
+                                           refinements: refinements,
+                                           disjunctiveFacets: [])
+    let queries = helper.makeQueries()
+    XCTAssertEqual(queries.count, 1)
+    XCTAssertEqual(queries.first?.filters, """
+    ("brand":"apple" AND "brand":"samsung" AND "brand":"sony") AND ("color":"blue" AND "color":"green" AND "color":"red") AND ("size":"m" AND "size":"s")
+    """)
+  }
+  
+  func testMakeQueriesDisjunctiveSingle() {
+    let refinements: [Attribute: [String]] = [
+      "size": ["m", "s"],
+      "color": ["blue", "green", "red"],
+      "brand": ["apple", "samsung", "sony"]
+    ]
+
+    let helper = DisjunctiveFacetingHelper(query: Query(),
+                                           refinements: refinements,
+                                           disjunctiveFacets: ["color"])
+    let queries = helper.makeQueries()
+    XCTAssertEqual(queries.count, 2)
+    XCTAssertEqual(queries.first?.filters, """
+    ("brand":"apple" AND "brand":"samsung" AND "brand":"sony") AND ("color":"blue" OR "color":"green" OR "color":"red") AND ("size":"m" AND "size":"s")
+    """)
+    XCTAssertEqual(queries.last?.facets, ["color"])
+    XCTAssertEqual(queries.last?.filters, """
+    ("brand":"apple" AND "brand":"samsung" AND "brand":"sony") AND ("size":"m" AND "size":"s")
+    """)
+  }
+  
+  func testMakeQueriesDisjunctiveDouble() {
+    let refinements: [Attribute: [String]] = [
+      "size": ["m", "s"],
+      "color": ["blue", "green", "red"],
+      "brand": ["apple", "samsung", "sony"]
+    ]
+    let disjunctiveFacets: Set<Attribute> = [
+      "color",
+      "size"
+    ]
+    let helper = DisjunctiveFacetingHelper(query: Query(),
+                                           refinements: refinements,
+                                           disjunctiveFacets: disjunctiveFacets)
+    let queries = helper.makeQueries()
+    XCTAssertEqual(queries.count, 3)
+    XCTAssertEqual(queries.first?.filters, """
+    ("brand":"apple" AND "brand":"samsung" AND "brand":"sony") AND ("color":"blue" OR "color":"green" OR "color":"red") AND ("size":"m" OR "size":"s")
+    """)
+    XCTAssertEqual(queries[1].facets, ["color"])
+    XCTAssertEqual(queries[1].filters, """
+    ("brand":"apple" AND "brand":"samsung" AND "brand":"sony") AND ("size":"m" OR "size":"s")
+    """)
+    XCTAssertEqual(queries[2].facets, ["size"])
+    XCTAssertEqual(queries[2].filters, """
+    ("brand":"apple" AND "brand":"samsung" AND "brand":"sony") AND ("color":"blue" OR "color":"green" OR "color":"red")
+    """)
+  }
+  
+  func testMergeEmptyResponses() {
+    let refinements: [Attribute: [String]] = [
+      "size": ["m", "s"],
+      "color": ["blue", "green", "red"],
+      "brand": ["apple", "samsung", "sony"]
+    ]
+    let disjunctiveFacets: Set<Attribute> = [
+      "color",
+      "size"
+    ]
+    let helper = DisjunctiveFacetingHelper(query: Query(),
+                                           refinements: refinements,
+                                           disjunctiveFacets: disjunctiveFacets)
+    
+    XCTAssertThrowsError(try helper.mergeResponses([]))
+  }
+  
+  func testMergeDisjunctiveSingle() throws {
+    let refinements: [Attribute: [String]] = [
+      "size": ["m", "s"],
+      "color": ["blue", "green", "red"],
+      "brand": ["apple", "samsung", "sony"]
+    ]
+    let disjunctiveFacets: Set<Attribute> = [
+      "color"
+    ]
+    let helper = DisjunctiveFacetingHelper(query: Query(),
+                                           refinements: refinements,
+                                           disjunctiveFacets: disjunctiveFacets)
+    var mainResponse = SearchResponse()
+    mainResponse.facets = [
+      "size": [
+        Facet(value: "s", count: 5),
+        Facet(value: "m", count: 7),
+      ],
+      "color": [
+        Facet(value: "red", count: 1),
+        Facet(value: "green", count: 2),
+        Facet(value: "blue", count: 3),
+      ],
+      "brand": [
+        Facet(value: "samsung", count: 5),
+        Facet(value: "sony", count: 10),
+        Facet(value: "apple", count: 15),
+      ],
+    ]
+    var disjunctiveResponse = SearchResponse()
+    disjunctiveResponse.facets = [
+      "color": [
+        Facet(value: "red", count: 10),
+        Facet(value: "green", count: 20),
+        Facet(value: "blue", count: 30),
+      ]
+    ]
+    let response = try helper.mergeResponses([
+      mainResponse,
+      disjunctiveResponse,
+    ])
+    XCTAssertEqual(response.facets, [
+      "size": [
+        Facet(value: "s", count: 5),
+        Facet(value: "m", count: 7),
+      ],
+      "color": [
+        Facet(value: "red", count: 1),
+        Facet(value: "green", count: 2),
+        Facet(value: "blue", count: 3),
+      ],
+      "brand": [
+        Facet(value: "samsung", count: 5),
+        Facet(value: "sony", count: 10),
+        Facet(value: "apple", count: 15),
+      ],
+    ])
+    XCTAssertEqual(response.disjunctiveFacets, ["color": [
+      Facet(value: "red", count: 10),
+      Facet(value: "green", count: 20),
+      Facet(value: "blue", count: 30),
+    ]])
+  }
+  
+  func testMergeDisjunctiveDouble() throws {
+    let refinements: [Attribute: [String]] = [
+      "size": ["m", "s"],
+      "color": ["blue", "green", "red"],
+      "brand": ["apple", "samsung", "sony"]
+    ]
+    let disjunctiveFacets: Set<Attribute> = [
+      "color",
+      "size"
+    ]
+    let helper = DisjunctiveFacetingHelper(query: Query(),
+                                           refinements: refinements,
+                                           disjunctiveFacets: disjunctiveFacets)
+    var mainResponse = SearchResponse()
+    mainResponse.facets = [
+      "size": [
+        Facet(value: "s", count: 5),
+        Facet(value: "m", count: 7),
+      ],
+      "color": [
+        Facet(value: "red", count: 1),
+        Facet(value: "green", count: 2),
+        Facet(value: "blue", count: 3),
+      ],
+      "brand": [
+        Facet(value: "samsung", count: 5),
+        Facet(value: "sony", count: 10),
+        Facet(value: "apple", count: 15),
+      ],
+    ]
+    var firstDisjunctiveResponse = SearchResponse()
+    firstDisjunctiveResponse.facets = [
+      "color": [
+        Facet(value: "red", count: 10),
+        Facet(value: "green", count: 20),
+        Facet(value: "blue", count: 30),
+      ]
+    ]
+    var secondDisjunctiveResponse = SearchResponse()
+    secondDisjunctiveResponse.facets = [
+      "size": [
+        Facet(value: "s", count: 3),
+        Facet(value: "m", count: 4),
+      ]
+    ]
+    let response = try helper.mergeResponses([
+      mainResponse,
+      firstDisjunctiveResponse,
+      secondDisjunctiveResponse,
+    ])
+    XCTAssertEqual(response.facets, [
+      "size": [
+        Facet(value: "s", count: 5),
+        Facet(value: "m", count: 7),
+      ],
+      "color": [
+        Facet(value: "red", count: 1),
+        Facet(value: "green", count: 2),
+        Facet(value: "blue", count: 3),
+      ],
+      "brand": [
+        Facet(value: "samsung", count: 5),
+        Facet(value: "sony", count: 10),
+        Facet(value: "apple", count: 15),
+      ],
+    ])
+    XCTAssertEqual(response.disjunctiveFacets, [
+      "color": [
+        Facet(value: "red", count: 10),
+        Facet(value: "green", count: 20),
+        Facet(value: "blue", count: 30),
+      ],
+      "size": [
+        Facet(value: "s", count: 3),
+        Facet(value: "m", count: 4),
+      ],
+    ])
+  }
+  
+  func testMergeFacetStats() throws {
+    let helper = DisjunctiveFacetingHelper(query: Query(),
+                                           refinements: [:],
+                                           disjunctiveFacets: [])
+    
+    
+    var mainResponse = SearchResponse()
+    mainResponse.facetStats = [
+      "price": FacetStats(min: 5, max: 100, avg: 52.5, sum: 2400),
+      "target": FacetStats(min: 1, max: 10, avg: 5.5, sum: 43)
+    ]
+    var firstDisjunctiveResponse = SearchResponse()
+    firstDisjunctiveResponse.facetStats = [
+      "price": FacetStats(min: 7, max: 103, avg: 55, sum: 3000),
+      "note": FacetStats(min: 1, max: 5, avg: 3, sum: 37)
+    ]
+    var secondDisjunctiveResponse = SearchResponse()
+    secondDisjunctiveResponse.facetStats = [
+      "size": FacetStats(min: 20, max: 56, avg: 38, sum: 242)
+    ]
+    let response = try helper.mergeResponses([
+      mainResponse,
+      firstDisjunctiveResponse,
+      secondDisjunctiveResponse,
+    ])
+    
+    
+    XCTAssertEqual(response.facetStats?.count, 4)
+    assertEqual(response.facetStats?["price"], FacetStats(min: 7, max: 103, avg: 55, sum: 3000),
+                file: #filePath,
+                line: #line)
+    assertEqual(response.facetStats?["target"], FacetStats(min: 1, max: 10, avg: 5.5, sum: 43),
+                file: #filePath,
+                line: #line)
+    assertEqual(response.facetStats?["size"], FacetStats(min: 20, max: 56, avg: 38, sum: 242),
+                file: #filePath,
+                line: #line)
+    assertEqual(response.facetStats?["note"], FacetStats(min: 1, max: 5, avg: 3, sum: 37),
+                file: #filePath,
+                line: #line)
+  }
+  
+  func assertEqual(_ lhs: FacetStats?, _ rhs: FacetStats?, file: StaticString = #filePath, line: UInt = #line) {
+    guard let lhs = lhs else {
+      XCTAssertNil(rhs, file: file, line: line)
+      return
+    }
+    guard let rhs = rhs else {
+      XCTAssertNil(lhs, file: file, line: line)
+      return
+    }
+    XCTAssertEqual(lhs.min, rhs.min, accuracy: 0.01, file: file, line: line)
+    XCTAssertEqual(lhs.max, rhs.max, accuracy: 0.01, file: file, line: line)
+    if let lAvg = lhs.avg, let rAvg = rhs.avg {
+      XCTAssertEqual(lAvg, rAvg, accuracy: 0.01, file: file, line: line)
+    } else {
+      XCTAssertEqual(lhs.avg, rhs.avg, file: file, line: line)
+    }
+    if let lSum = lhs.sum, let rSum = rhs.sum {
+      XCTAssertEqual(lSum, rSum, accuracy: 0.01, file: file, line: line)
+    } else {
+      XCTAssertEqual(lhs.sum, rhs.sum, file: file, line: line)
+    }
+  }
+  
+  func testMergeExhaustiveFacetsCount() throws {
+    let helper = DisjunctiveFacetingHelper(query: Query(),
+                                           refinements: [:],
+                                           disjunctiveFacets: [])
+    
+    
+    var mainResponse = SearchResponse()
+    mainResponse.exhaustiveFacetsCount = true
+    var firstDisjunctiveResponse = SearchResponse()
+    firstDisjunctiveResponse.exhaustiveFacetsCount = true
+    var secondDisjunctiveResponse = SearchResponse()
+    secondDisjunctiveResponse.exhaustiveFacetsCount = false
+    var response = try helper.mergeResponses([
+      mainResponse,
+      firstDisjunctiveResponse,
+      secondDisjunctiveResponse,
+    ])
+    XCTAssertFalse(response.exhaustiveFacetsCount!)
+    secondDisjunctiveResponse.exhaustiveFacetsCount = true
+    response = try helper.mergeResponses([
+      mainResponse,
+      firstDisjunctiveResponse,
+      secondDisjunctiveResponse,
+    ])
+    XCTAssertTrue(response.exhaustiveFacetsCount!)
+  }
+
+}
